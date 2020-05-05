d5861c9f68c3f05f0aa6100bc008b9a2056c5d75
giladby <56963755+giladby@users.noreply.github.com>
2020-05-05 18:23:12 +0300

    Add project files.

---
+++ FlightControlWeb/Models/Flight.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FlightControlWeb.Models
+{
+    public class Flight
+    {
+        public string FlightId { get; set; }
+        public double Longtitude { get; set; }
+        public double Latitude { get; set; }
+        public int Passangers { get; set; }
+        public string CompanyName { get; set; }
+        public DateTime DateTime { get; set; }
+        public bool IsExternal { get; set; }
+    }
+}
