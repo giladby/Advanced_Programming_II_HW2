41671611b346a32cd581a43b9004d86aefa5349d
Yuval Kittenplon <yuval.kittenplon@live.biu.ac.il>
2020-05-20 21:13:53 +0300

    error fade

--- FlightControlWeb/wwwroot/JavaScripts/Utils.js
+++ FlightControlWeb/wwwroot/JavaScripts/Utils.js
@@ -1,22 +1,18 @@
 ﻿async function printError(error) {
 
-    console.log("enter");
-
     var msg = document.getElementById("myErrorMsg");
-    msg.style.opacity = "1";
     msg.innerHTML = error;
+
+    $("#myErrorMsg").fadeIn(0);
     let width = msg.offsetWidth;
     let offset = -0.5 * width;
-    let offsetString = offset.toString() + "px"
+    let offsetString = offset.toString() + "px";
     msg.style.marginLeft = offsetString;
-    msg.style.visibility = "visible";
-
-    await Sleep(5000);
-
-    // Set the opacity of div to 0 (transparent)
-    msg.style.opacity = "0";
-
-    msg.style.visibility = "hidden";
+    $("#myErrorMsg").fadeOut(0);
+    
+    $("#myErrorMsg").fadeIn();
+    await Sleep(3000);
+    $("#myErrorMsg").fadeOut();
 }
 
 function Sleep(ms) {
