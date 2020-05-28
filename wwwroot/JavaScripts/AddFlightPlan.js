322018a92ee2827aa892655d2bb877e9ffdbcf4d
giladby <56963755+giladby@users.noreply.github.com>
2020-05-28 11:55:51 +0300

    handling compilation

---
+++ wwwroot/JavaScripts/AddFlightPlan.js
@@ -0,0 +1,14 @@
+﻿let flightPlanUrl = "../api/FlightPlan";
+// send a 'POST' request with the given flight plan to the server
+function addFlightPlanFunc(flightPlan) {
+    $.ajax({
+        type: 'post',
+        url: flightPlanUrl,
+        data: JSON.stringify(flightPlan),
+        contentType: "application/json; charset=utf-8",
+        traditional: true,
+        error: function () {
+            printError("Received invalid flight plan");
+        }
+    });
+}
